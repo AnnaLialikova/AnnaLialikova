9450c453eb93a559c9f4f0d35056fe00fd533469
Ляликова Анна <65788392+AnnaLialikova@users.noreply.github.com>
2020-05-25 20:40:08 +0300

    Add files via upload

---
+++ 5/Greenpeace/Greenpeace/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Greenpeace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(320, 218);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(137, 42);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Greenpease";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonExit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ 5/Greenpeace/Greenpeace/Form2Start.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Greenpeace
+{
+    partial class Form2Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenForm2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpenForm2
+            // 
+            this.buttonOpenForm2.Location = new System.Drawing.Point(292, 127);
+            this.buttonOpenForm2.Name = "buttonOpenForm2";
+            this.buttonOpenForm2.Size = new System.Drawing.Size(199, 91);
+            this.buttonOpenForm2.TabIndex = 0;
+            this.buttonOpenForm2.Text = "Open next form";
+            this.buttonOpenForm2.UseVisualStyleBackColor = true;
+            this.buttonOpenForm2.Click += new System.EventHandler(this.buttonOpenForm2_Click);
+            // 
+            // Form2Start
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonOpenForm2);
+            this.Name = "Form2Start";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2Start";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenForm2;
+    }
+}
